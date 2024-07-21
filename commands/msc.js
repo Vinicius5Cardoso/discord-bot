58ed6a89a5e5bc4fd7eba940242b5e8b39d162a8
younk <142642244+younk5@users.noreply.github.com>
2024-07-21 10:43:29 -0300

    Update msc.js

--- commands/msc.js
+++ commands/msc.js
@@ -10,17 +10,13 @@ module.exports = {
             return message.channel.send('**Este comando não é permitido neste canal.**');
         }
 
-        // Cria um novo embed
         const embed = new EmbedBuilder()
-            .setColor('#0099ff') // Define a cor da borda do embed
+            .setColor('#0099ff') 
             .setTitle('Uso do comando !d')
             .setDescription('**Uso correto do comando !d:** `!d <link> <formato>`\n\n**Baixa um vídeo ou áudio do YouTube e envia o arquivo no chat.**\n\n**Formato disponível:** `audio` ou `video`.\n\n**[Twitter](https://x.com/NotYounk) [GitHub](https://github.com/younk5).**')
-            .setTimestamp(); // Adiciona a data e hora atuais
-
-        // Envia o embed para o canal
+            .setTimestamp(); 
         const responseMessage = await message.channel.send({ embeds: [embed] });
 
-        // Apaga a mensagem com o comando digitado
         await message.delete();
     },
 };
